f1e53fdda3a062ea33c9997daa62937dcff6daf7
diayal-senegal <papaoumardeme@gmail.com>
2026-01-17 16:29:36 +0100

    Mise à jour texte de la page pending

--- src/views/Pending.jsx
+++ src/views/Pending.jsx
@@ -4,8 +4,8 @@ const Pending = () => {
     return (
         <div className='flex justify-center items-center min-h-screen bg-gray-100'>
             <div className='text-center'>
-                <h1 className='text-4xl font-bold text-yellow-600 mb-4'>En attente</h1>
-                <h2 className='text-2xl font-semibold mb-4'>Compte en cours de validation</h2>
+                <h1 className='text-4xl font-bold text-yellow-600 mb-4'>Compte en attente de validation</h1>
+                <h2 className='text-2xl font-semibold mb-4'> </h2>
                 <p className='text-gray-600 mb-6'>Votre compte vendeur est en cours de validation par notre équipe.</p>
                 <p className='text-gray-600'>Vous recevrez un email dès que votre compte sera activé.</p>
             </div>
